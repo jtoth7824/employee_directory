1b17ab826c1c9633e0250fab38f760d95745c011
LostInSpace <71476446+jtoth7824@users.noreply.github.com>
2021-03-28 13:21:11 -0400

    applied some basic styling

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,30 +3,36 @@ import "./style.css";
 
 function Card(props) {
   return (
+//    props.results.map(result => (
+//      <div className="row">
+//   <div className="card">
+        <table className="table table-curved table-responsive">
+          <thead>
+            <tr>
+              <th className="alignCenter">Pic</th>
+              <th className="alignCenter"><span onClick={() => props.sortField("first")}>First Name</span></th>
+              <th className="alignCenter"><span onClick={() => props.sortField("last")}>Last Name</span></th>
+              <th className="alignCenter"><span onClick={() => props.sortField("email")}>Email</span></th>
+              <th className="alignCenter"><span onClick={() => props.sortField("phone")}>Phone</span></th>
+            </tr>
+          </thead>
+          {props.results.map(result => (
+          <tbody key={result.cell}>
 
-      <table className="table-responsive">
-        <thead>
-          <tr>
-            <th>Pic</th>
-            <th><span onClick={() => props.sortField("first")}>First Name</span></th>
-            <th><span onClick={() => props.sortField("last")}>Last Name</span></th>
-            <th><span onClick={() => props.sortField("email")}>Email</span></th>
-            <th><span onClick={() => props.sortField("phone")}>Phone</span></th>
-          </tr>
-        </thead>
-      {props.results.map(result => (
-        <tbody key={result.cell}>
-          <tr>
-            <td><img src={result.picture.medium} alt="employee pic"/></td>
-            <td><p>{result.name.first}  </p></td>
-            <td><p>{result.name.last}   </p></td>
-            <td><p>{result.email}   </p></td>
-            <td><p>{result.phone}   </p></td>
-          </tr>
-        </tbody>
-      ))}
-    </table>
-  );
+
+            <tr className="blue">
+              <td className="align-middle text-center"><img src={result.picture.medium} alt="employee pic"/></td>
+              <td className="align-middle text-center"><p>{result.name.first}</p></td>
+              <td className="align-middle text-center"><p>{result.name.last}</p></td>
+              <td className="align-middle text-center"><p>{result.email}</p></td>
+              <td className="align-middle text-center"><p>{result.phone}</p></td>
+            </tr>
+          </tbody>
+          ))}
+        </table>
+//      </div>
+// </div>
+//    ))
+  )
 }
-
 export default Card;
