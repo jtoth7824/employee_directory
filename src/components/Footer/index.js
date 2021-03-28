b4aa6b728e8db57c9954691ca769b91b22034632
LostInSpace <71476446+jtoth7824@users.noreply.github.com>
2021-03-28 17:30:28 -0400

    added footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,9 +3,21 @@ import "./style.css";
 
 function Footer() {
   return (
-    <footer className="footer">
-      <span>Pupster 2017</span>
-    </footer>
+<section className="container">
+    <h2 className="invisible">Container for the sticky footer</h2>
+    <section className="fixed-bottom">
+        <h3 className="invisible">Bootstrap grid containing footer</h3>
+        <section className="row">
+            <h4 className="invisible">First row of footer grid</h4>
+            <section className="col">
+                <h5 className="invisible">Footer for page</h5>
+                <footer className="myFooter brand text-left">
+                    &copy; John Toth 2021
+                </footer>
+            </section>
+        </section>
+    </section>
+</section>
   );
 }
 
